98c9fe0beab18d5a03443b7b9d419d4cbc641981
MichalRac <rac.michalmail@gmail.com>
2022-03-18 15:45:50 +0100

    Complete task 2 - reading file and directory structure and displaying it

---
+++ FileExplorer-MichalRac/FileExplorer-MichalRac/DirectoryTreeViewItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace FileExplorer_MichalRac
+{
+    public class DirectoryTreeViewItem
+    {
+        public string? Title { get; set; }
+        public ObservableCollection<DirectoryTreeViewItem> Items { get; set; }
+
+        public DirectoryTreeViewItem(string path, bool isDirectory)
+        {
+            try
+            {
+                Title = Path.GetFileName(path);
+                Items = isDirectory 
+                    ? ReadFilesFromDirectory(path) 
+                    : new ObservableCollection<DirectoryTreeViewItem>();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                throw;
+            }
+        }
+
+        public ObservableCollection<DirectoryTreeViewItem> ReadFilesFromDirectory(string path)
+        {
+            var result = new ObservableCollection<DirectoryTreeViewItem>();
+            var directoryPaths = Directory.GetDirectories(path);
+
+            foreach (var directoryPath in directoryPaths)
+            {
+                var dtvi = new DirectoryTreeViewItem(directoryPath, true);
+                result.Add(dtvi);
+            }
+
+            var filePaths = Directory.GetFiles(path);
+            foreach(var filePath in filePaths)
+            {
+                result.Add(new DirectoryTreeViewItem(filePath, false));
+            }
+
+            return result;
+        }
+    }
+}
