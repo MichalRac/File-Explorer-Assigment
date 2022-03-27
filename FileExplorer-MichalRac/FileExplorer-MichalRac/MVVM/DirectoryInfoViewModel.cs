c9e0b6a827644737ae9b6df15ef67e51df2ed959
MichalRac <rac.michalmail@gmail.com>
2022-03-27 05:24:53 +0200

    [Lab2 - MVVM] Complete task 1.3 - Fill collections

--- FileExplorer-MichalRac/FileExplorer-MichalRac/MVVM/DirectoryInfoViewModel.cs
+++ FileExplorer-MichalRac/FileExplorer-MichalRac/MVVM/DirectoryInfoViewModel.cs
@@ -3,12 +3,42 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
     public class DirectoryInfoViewModel : FileSystemInfoViewModel
     {
+        public Exception Exception { get; private set; }
         public ObservableCollection<FileSystemInfoViewModel> Items { get; private set; } = new ObservableCollection<FileSystemInfoViewModel>();
+
+        public bool Open(string path)
+        {
+            bool result = false;
+            try
+            {
+                foreach(var dirName in Directory.GetDirectories(path))
+                {
+                    var dirInfo = new DirectoryInfo(dirName);
+                    var itemViewModel = new DirectoryInfoViewModel();
+                    itemViewModel.Model = dirInfo;
+                    Items.Add(itemViewModel);
+                }
+                foreach (var fileName in Directory.GetFiles(path))
+                {
+                    var fileInfo = new FileInfo(path);
+                    var itemViewModel = new FileInfoViewModel();
+                    itemViewModel.Model = fileInfo;
+                    Items.Add(itemViewModel);
+                }
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Exception = ex;
+            }
+            return result;
+        }
     }
 }
