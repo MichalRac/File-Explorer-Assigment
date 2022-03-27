b252d83ef12403d1dc455348507123dc5f7c4f87
MichalRac <rac.michalmail@gmail.com>
2022-03-27 05:26:55 +0200

    [Lab2 - MVVM] Complete task 1.4 - Implement file explorer class

---
+++ FileExplorer-MichalRac/FileExplorer-MichalRac/MVVM/FileExplorer.cs
@@ -0,0 +1,19 @@
+﻿namespace FileExplorer_MichalRac.MVVM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FileExplorer : ViewModelBase
+    {
+        public DirectoryInfoViewModel Root { get; set; }
+
+        public void OpenRoot(string path)
+        {
+            Root = new DirectoryInfoViewModel();
+            Root.Open(path);
+        }
+    }
+}
