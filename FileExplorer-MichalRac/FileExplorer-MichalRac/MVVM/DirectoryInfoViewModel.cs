92a1bf0a409e8493ca1b106d4a934d97d641679d
MichalRac <rac.michalmail@gmail.com>
2022-03-27 05:18:43 +0200

    [Lab2 - MVVM] Complete task 1.2 - Declare observable ViewModel collection

--- FileExplorer-MichalRac/FileExplorer-MichalRac/MVVM/DirectoryInfoViewModel.cs
+++ FileExplorer-MichalRac/FileExplorer-MichalRac/MVVM/DirectoryInfoViewModel.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
     public class DirectoryInfoViewModel : FileSystemInfoViewModel
     {
+        public ObservableCollection<FileSystemInfoViewModel> Items { get; private set; } = new ObservableCollection<FileSystemInfoViewModel>();
     }
 }
